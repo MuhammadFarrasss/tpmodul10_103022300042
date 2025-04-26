acef567ea77c0ad7b4583d12aa99e452e41616a7
MuhammadFarrasss <muhammadfarraaas@gmail.com>
2025-04-27 00:55:38 +0700

    menambahkan API mahasiswa

---
+++ Mahasiswa.cs
@@ -0,0 +1,11 @@
+﻿namespace tpmodul10_103022300042
+{
+    using System;
+
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string Nim { get; set; }
+    }
+
+}
